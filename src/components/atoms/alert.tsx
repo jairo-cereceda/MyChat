9e532d32de73b40528ab75725a504f14def75697
Jairo <jaiiro.cereceda@gmail.com>
2026-02-14 22:59:21 +0100

    fix: alert buttons position

--- src/components/atoms/alert.tsx
+++ src/components/atoms/alert.tsx
@@ -15,16 +15,16 @@ function Alert({ text, onConfirm, onCancel }: AlertProps) {
         <h2 className="font-semibold text-center">{text}</h2>
         <div className="flex justify-between w-full gap-4">
           <button
-            onClick={onConfirm}
-            className="font-semibold px-5 py-2 rounded-lg bg-detail hover:bg-detail-hover active:bg-detail-hover"
-          >
-            Aceptar
-          </button>
-          <button
             onClick={onCancel}
             className="font-semibold px-5 py-2 rounded-lg bg-detail hover:bg-detail-hover active:bg-detail-hover"
           >
             Cancelar
+          </button>
+          <button
+            onClick={onConfirm}
+            className="font-semibold px-5 py-2 rounded-lg bg-detail hover:bg-detail-hover active:bg-detail-hover"
+          >
+            Aceptar
           </button>
         </div>
       </div>
